6ec8dd9f805f8e889da557616b9328ca6e83144f
Peter Jihoon Kim <raingrove@gmail.com>
2017-03-31 00:28:46 -0700

    new xcode project

---
+++ WiFiMenu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WiFiMenu
+//
+//  Created by Peter Jihoon Kim on 3/31/17.
+//  Copyright © 2017 Peter Jihoon Kim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
